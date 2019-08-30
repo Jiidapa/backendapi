32b35dfa3de26dec60b9800363c536ff9b22a3f0
jiidapa <jidapa.p@kkumail.com>
2019-08-30 17:00:10 +0700

    change page

---
+++ public/precache-manifest.d045e7c7bb3d3b7223409722265614c5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186f504443c591f85d16c07226853f3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed370e99ea86b52e924f",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6d631a1a82e415ca3cef",
+    "url": "/static/css/main.30086e5c.chunk.css"
+  },
+  {
+    "revision": "ed370e99ea86b52e924f",
+    "url": "/static/js/2.83fddeb8.chunk.js"
+  },
+  {
+    "revision": "6d631a1a82e415ca3cef",
+    "url": "/static/js/main.079d4c0f.chunk.js"
+  },
+  {
+    "revision": "ceb15504e89d88b7c521",
+    "url": "/static/js/runtime~main.42af176a.js"
+  },
+  {
+    "revision": "45e50cd0f6a5f26e86943e95e35c1e29",
+    "url": "/static/media/easy1.45e50cd0.jpg"
+  },
+  {
+    "revision": "618b65d80273f053d3c9cf16d7b4c56d",
+    "url": "/static/media/easy2.618b65d8.jpg"
+  },
+  {
+    "revision": "06660c3751759bec3b08b42a40bc0931",
+    "url": "/static/media/easy3.06660c37.jpg"
+  },
+  {
+    "revision": "474f86f0dffe390a5a0b7f61fe2fd7f1",
+    "url": "/static/media/easy4.474f86f0.jpg"
+  },
+  {
+    "revision": "cbafa58ce396bdc247f004daf6ad136a",
+    "url": "/static/media/jida.cbafa58c.jpg"
+  },
+  {
+    "revision": "2caca8c6254b61ecc1a4c05993b97f1c",
+    "url": "/static/media/krixxi1.2caca8c6.jpg"
+  },
+  {
+    "revision": "81e73a1c4a2d84e93f347925028a9a87",
+    "url": "/static/media/krixxi2.81e73a1c.jpg"
+  },
+  {
+    "revision": "ff06e185bcee283ebb9430b58ddc303a",
+    "url": "/static/media/logo.ff06e185.png"
+  },
+  {
+    "revision": "aed3c0b8098ec49a9e9d16e2b7a33be4",
+    "url": "/static/media/pig1.aed3c0b8.jpg"
+  },
+  {
+    "revision": "0eb2da6747a9680d408c7c8496bda9d5",
+    "url": "/static/media/pig2.0eb2da67.JPG"
+  },
+  {
+    "revision": "a44523eaa06c604fd3630eacb4bf87cf",
+    "url": "/static/media/pig3.a44523ea.jpg"
+  }
+]);
